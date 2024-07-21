ee59094a9e4066d37fd5f9f107be7692ee9ba357
kenchirchir <k.chirchir@alustudent.com>
2024-07-21 18:32:44 +0200

    Log parsing

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,37 +1,50 @@
 #!/usr/bin/python3
-""" reads stdin line by line and computes metrics:
-"""
-import sys
+"""Reads from standard input and computes metrics."""
 
-file_size = 0
-status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
-               "403": 0, "404": 0, "405": 0, "500": 0}
-iter = 0
-try:
-    for line in sys.stdin:
-        iter += 1
-        line_list = line.split()
-        if len(line_list) >= 2:
-            if line_list[-2] in status_code:
-                status_code[line_list[-2]] += 1
+
+def print_statistics(total_size, status_code_counts):
+    """Print accumulated metrics.
+    Args:
+        total_size (int): The accumulated total file size.
+        status_code_counts (dict): The accumulated count of status codes.
+    """
+    print("Total file size: {}".format(total_size))
+    for status_code, count in status_code_counts.items():
+        print("{}: {}".format(status_code, count))
+
+
+if __name__ == "__main__":
+    import sys
+
+    total_size = 0
+    status_code_counts = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    line_count = 0
+    try:
+        for line in sys.stdin:
+            if line_count == 10:
+                print_statistics(total_size, status_code_counts)
+                line_count = 1
+            else:
+                line_count += 1
+            words = line.split()
             try:
-                file_size += int(line_list[-1])
-            except FileNotFoundError:
+                size = int(words[-1])
+                total_size += size
+            except (IndexError, ValueError):
+                pass
+            try:
+                status_code = words[-2]
+                if status_code in valid_status_codes:
+                    if status_code not in status_code_counts:
+                        status_code_counts[status_code] = 1
+                    else:
+                        status_code_counts[status_code] += 1
+            except IndexError:
                 pass
 
-        if iter % 10 == 0:
-            print(f"File size: {file_size:d}")
-            for key, value in sorted(status_code.items()):
-                if value:
-                    print(f"{key}: {value:d}")
+        print_statistics(total_size, status_code_counts)
 
-    print(f"File size: {file_size:d}")
-    for key, value in sorted(status_code.items()):
-        if value:
-            print(f"{key}: {value:d}")
-
-except KeyboardInterrupt:
-    print(f"File size: {file_size:d}")
-    for key, value in sorted(status_code.items()):
-        if value:
-            print(f"{key}: {value:d}")
+    except KeyboardInterrupt:
+        print_statistics(total_size, status_code_counts)
+        raise
